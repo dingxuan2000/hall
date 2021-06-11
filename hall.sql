e8d9623e9683c4154d4f8b811514b06d52214a33
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2021-06-11 18:23:40 +0800

    Create: add mysql example

--- hall.sql
+++ hall.sql
@@ -2,3 +2,13 @@ drop database if exists hall;
 
 create database hall default character set utf8mb4 collate utf8mb4_unicode_ci;
 use hall;
+
+CREATE TABLE user
+(
+    id int not null auto_increment,
+    name varchar(20),
+    age int,
+    primary key (id)
+);
+
+insert into user values (1,'ads', 18);
---
+++ src/main/java/com/example/hall/mysql/UserController.java
@@ -0,0 +1,46 @@
+package com.example.hall.mysql;
+
+import freemarker.cache.StrongCacheStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserDao userDao;
+
+//    http://localhost:8080/getAll
+    @GetMapping("/getAll")
+    public List getAll(){
+        List all = userDao.findAll();
+        return all;
+    }
+
+//    http://localhost:8080/add?name=asdasd&age=13  用postman测试
+    @PostMapping("/add")
+    public User add(@RequestParam("name") String name, @RequestParam("age") int age){
+        User user =new User();
+        user.setName(name);
+        user.setAge(age);
+
+        User save = userDao.save(user);
+        return save;
+    }
+
+    //    http://localhost:8080/deleteOne?id=1  用postman测试
+    @DeleteMapping("/deleteOne")
+    public String deleteOne(@RequestParam("id") int id){
+        userDao.deleteById(id);
+        return "delete";
+    }
+
+    @GetMapping("/deleteAll")
+    public String deleteAll(){
+        userDao.deleteAll();
+        return "delete All";
+    }
+
+}
