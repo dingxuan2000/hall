7703cb6ba9f961e4780448ee9586e9f5721566a6
s1kan <sanoton1995@gmail.com>
2021-06-24 09:35:53 +0800

    updateById in test file & updates in hall.sql

--- hall.sql
+++ hall.sql
@@ -14,7 +14,7 @@ CREATE TABLE user
 insert into user values (1,'ads', 18);
 insert into user values (2,'xuan', 21);
 
--- 丁璇
+-- 丁璇 社区管理
 create table comm_manager
 (
     comm_id int not null auto_increment,
@@ -25,31 +25,44 @@ create table comm_manager
 insert into comm_manager values (1,'commTest1');
 insert into comm_manager values (2,'commTest2');
 
--- lucy & huang
+-- lucy & huang 小区管理
 create table house_manager
 (
     house_id int not null auto_increment,
     house_name varchar(255) not null comment '小区名称',
     comm_id int not null,
-    foreign key (comm_id) references comm_manager(comm_id) ON DELETE CASCADE ON UPDATE CASCADE,
-#     foreign key (comm_id) references comm_manager(comm_id),
+    comm_name varchar(255) not null comment '社区名称',
     primary key (house_id)
 );
 
-insert into house_manager values (1,'houseTest1', 1);
-insert into house_manager values (2,'houseTest2', 2);
+insert into house_manager values (1,'houseTest1', 1, 'commTest1');
+insert into house_manager values (2,'houseTest2', 2, 'commTest2');
 
--- steven
+-- steven 收费类型
 create table charge_type
 (
     charge_id int not null auto_increment,
-    charge_name varchar(255) not null comment '收费类型',
+    charge_name varchar(255) not null comment '收费类型名称',
     charge_unit varchar(255) not null comment '计价单位',
     primary key (charge_id)
 );
 
 insert into charge_type values (1,'electricity', 'xx');
 insert into charge_type values (2,'water', 'yy');
+
+-- steven
+create table utility_type
+(
+    utility_id int not null auto_increment,
+    charge_name varchar(255) not null comment '收费类型名称',
+    hall_name varchar(128) not null comment '所属喜宴厅',
+    hall_detail_floor varchar(32) not null comment '楼层',
+    utility_table_name varchar(128) not null comment '表名称',
+    utility_standard float not null comment '收费标准',
+    charge_unit varchar(255) not null comment '计价单位',
+    utility_rate int not null comment '倍率',
+    primary key(utility_id)
+);
 
 -- yang
 CREATE TABLE hall_manager
@@ -59,12 +72,12 @@ CREATE TABLE hall_manager
     hall_community varchar(128) not null,
     hall_house varchar(128) not null,
     hall_address varchar(128) not null,
-    hall_area int not null,
-    hall_table_num int not null,
+    hall_area int,
+    hall_table_num int,
     hall_deposit int not null,
-    primary key (hall_id)
+    primary key (hall_id, hall_name)
 );
-insert into hall_manager values (1,'ads', 'qwe', 'zxc', 'gdf', 1,2,3);
+insert into hall_manager values (1,'ads', 'qwe', 'zxc', 'gdf', 1, 2, 3);
 
 create table hall_manager_detail
 (
@@ -78,5 +91,4 @@ create table hall_manager_detail
     foreign key (hall_id) references hall_manager(hall_id),
     primary key (hall_detail_id)
 );
-
 
